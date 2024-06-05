435851885c4ed21a735e0f41d5ad18bb2c7fc700
CesarSoldier <brennoemmet@gmail.com>
2024-06-04 23:02:35 -0400

    04/06

--- app.py
+++ app.py
@@ -8,3 +8,26 @@ def home():
 
 if __name__ == '__main__':
     app.run(debug=True, port=8000)
+
+import sqlite3
+
+import click
+from flask import current_app, g
+
+
+def get_db():
+    if 'db' not in g:
+        g.db = sqlite3.connect(
+            current_app.config['DATABASE'],
+            detect_types=sqlite3.PARSE_DECLTYPES
+        )
+        g.db.row_factory = sqlite3.Row
+
+    return g.db
+
+
+def close_db(e=None):
+    db = g.pop('db', None)
+
+    if db is not None:
+        db.close()
