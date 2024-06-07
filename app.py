1dacb6221f272a57e13b336b42ab8cc742ca6bdf
CesarSoldier <brennoemmet@gmail.com>
2024-06-07 18:30:01 -0400

    day 07/06

--- app.py
+++ app.py
@@ -1,33 +1,39 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
+from config import Config
+from models import db, User
+import os
 
 app = Flask(__name__)
+app.config.from_object(Config)
+db.init_app(app)
 
 @app.route('/')
-def home():
-    return render_template('index.html')
+def index():
+    slide1 = ['girl1-left.png', 'girl1.png', 'girl1-right.png']
+    slide2 = ['girl2-left.png', 'girl2.png', 'girl2-right.png']
+    slide3 = ['girl3-left.png', 'girl3.png', 'girl3-right.png']
+    slides = [slide1, slide2, slide3]
+    return render_template('telaInicial.html', slides=slides)
+
+@app.route('/users', methods=['GET', 'POST'])
+def manage_users():
+    if request.method == 'POST':
+        username = request.form['username']
+        email = request.form['email']
+        new_user = User(username=username, email=email)
+        try:
+            db.session.add(new_user)
+            db.session.commit()
+            return redirect(url_for('manage_users'))
+        except Exception as e:
+            db.session.rollback()
+            return f"An error occurred while adding the user: {e}"
+    users = User.query.all()
+    return render_template('users.html', users=users)
+
 
 if __name__ == '__main__':
-    app.run(debug=True, port=8000)
-
-import sqlite3
-
-import click
-from flask import current_app, g
-
-
-def get_db():
-    if 'db' not in g:
-        g.db = sqlite3.connect(
-            current_app.config['DATABASE'],
-            detect_types=sqlite3.PARSE_DECLTYPES
-        )
-        g.db.row_factory = sqlite3.Row
-
-    return g.db
-
-
-def close_db(e=None):
-    db = g.pop('db', None)
-
-    if db is not None:
-        db.close()
+    with app.app_context():
+        if not os.path.exists(os.path.join(app.config['BASE_DIR'], 'app.db')):
+            db.create_all()
+    app.run(debug=True)
---
+++ config.py
@@ -0,0 +1,6 @@
+import os
+
+class Config:
+    BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+    SQLALCHEMY_DATABASE_URI = f"sqlite:///{os.path.join(BASE_DIR, 'app.db')}"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
