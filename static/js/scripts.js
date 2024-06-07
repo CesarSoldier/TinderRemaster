701f30f13b2fd2b16f74287b244b9d8bb1bc27eb
Iury Gabriel Faria <iury.gabriel.r@gmail.com>
2024-06-07 15:58:03 -0400

    Teste Tela Inicial

---
+++ static/js/scripts.js
@@ -0,0 +1,39 @@
+let slideIndex = 1;
+showSlides(slideIndex);
+
+let interval = setInterval(nextSlide, 3000);
+
+function nextSlide() {
+    showSlides(slideIndex += 1);
+}
+
+function prevSlide() {
+    showSlides(slideIndex -= 1);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let slides = document.getElementsByClassName("carousel-item");
+    let dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (let i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+
+
+}
+
+document.querySelectorAll('.prev, .next').forEach(item => {
+    item.addEventListener('click', () => {
+        clearInterval(interval);
+    });
+});
